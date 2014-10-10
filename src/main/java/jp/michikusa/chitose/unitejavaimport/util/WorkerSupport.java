f8489acfc3899afa9559630e8b36ed41096cf785
kamichidu <c.kamunagi@gmail.com>
2014-10-11 01:59:15 +0900

    Remove old files.

--- src/main/java/jp/michikusa/chitose/unitejavaimport/util/WorkerSupport.java
+++
@@ -1,8 +0,0 @@
-package jp.michikusa.chitose.unitejavaimport.util;
-
-public interface WorkerSupport<T>
-{
-    void addWorker(TaskWorker<? super T> worker);
-
-    void removeWorker(TaskWorker<? super T> worker);
-}
